bfe699c541ea61bf6d63e0d77da2f4803c0f3760
alexpri <alexandrpriad@gmail.com>
2015-09-21 07:43:25 +0300

    cookie

---
+++ js/form-validation.js
@@ -0,0 +1,32 @@
+(function () {
+    var formFilter = document.forms["upload-filter"],
+        filterInput =  formFilter.filter;
+
+    var restoreFormValueCookie = function(form) {
+        var element;
+
+        for (var i = 0; i < formFilter.elements.length; i++) {
+            element = formFilter.elements[i];
+
+
+            if (docCookies.hasItem(element.name)) {
+                element.value = docCookies.setItem(element.value);
+            }
+
+        }
+    }
+
+
+    var elementForm;
+
+    formFilter.onsubmit = function(evt) {
+        evt.preventDefault();
+
+        for (var i = 0; i < formFilter.elements.length; i++) {
+            element = formFilter.elements[i];
+
+            docCookies.setItem(element.name, element.value);
+        }
+    }
+
+})();
