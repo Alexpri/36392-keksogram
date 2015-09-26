8ef1201277fd0b5791e5fa41fba1b6eef4c86e26
alexpri <alexandrpriad@gmail.com>
2015-09-26 18:59:18 +0300

    validation cookie

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,81 +1,109 @@
 (function () {
-    var formResize = document.forms["upload-resize"],
-        resizeX = formResize["resize-x"],
-        resizeY = formResize["resize-y"],
-        resizeSize = formResize["resize-size"],
-        inputsStep2 =  document.querySelectorAll(".upload-resize-controls input");
 
-    var MAX_PHOTO_SIDE = 569;
-    var MIN_PHOTO_SIDE = 0;
-    var MIN_INPUT_RESIZE = 0;
+    var validation = {
 
-    resizeSize.min = MIN_PHOTO_SIDE;
-    resizeSize.max = MAX_PHOTO_SIDE;
-    resizeSize.value = MAX_PHOTO_SIDE;
-    resizeX.min = MIN_INPUT_RESIZE;
-    resizeY.min = MIN_INPUT_RESIZE;
-    resizeY.value = MIN_INPUT_RESIZE;
-    resizeX.value = MIN_INPUT_RESIZE;
+        init : function(){
+            var self = this;
+            console.log(1);
+            self.validation();
+            self.cookies();
+        },
 
-    var inputSize = function (field) {
+        validation: function () {
+            var formResize = document.forms["upload-resize"],
+                resizeX = formResize["resize-x"],
+                resizeY = formResize["resize-y"],
+                resizeSize = formResize["resize-size"],
+                inputsStep2 =  document.querySelectorAll(".upload-resize-controls input");
 
-            if (field == resizeX) {
+            var MAX_PHOTO_SIDE = 569;
+            var MIN_PHOTO_SIDE = 0;
+            var MIN_INPUT_RESIZE = 0;
 
-                field.onchange = function () {
-                    console.log(resizeX);
+            resizeSize.min = MIN_PHOTO_SIDE;
+            resizeSize.max = MAX_PHOTO_SIDE;
+            resizeSize.value = MAX_PHOTO_SIDE;
+            resizeX.min = MIN_INPUT_RESIZE;
+            resizeY.min = MIN_INPUT_RESIZE;
+            resizeY.value = MIN_INPUT_RESIZE;
+            resizeX.value = MIN_INPUT_RESIZE;
 
-                    resizeY.value = parseInt(field.value, 10);
-                    resizeSize.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
+            var inputSize = function (field) {
+
+                if (field == resizeX) {
+
+                    field.onchange = function () {
+
+                        resizeY.value = parseInt(field.value, 10);
+                        resizeSize.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
+                    }
+                } else if (field == resizeY) {
+                    field.onchange = function () {
+
+                        resizeX.value = parseInt(field.value, 10);
+                        resizeSize.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
+                    }
+                }  else if (field == resizeSize) {
+                    field.onchange = function () {
+
+                        resizeX.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
+                        resizeY.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
+                    }
                 }
-            } else if (field == resizeY) {
-                field.onchange = function () {
-                    console.log(resizeY);
+            };
 
-                    resizeX.value = parseInt(field.value, 10);
-                    resizeSize.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
-                }
-            }  else if (field == resizeSize) {
-                field.onchange = function () {
-                    console.log(resizeSize);
+            inputSize(resizeX);
+            inputSize(resizeY);
+            inputSize(resizeSize);
+        },
 
-                    resizeX.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
-                    resizeY.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
+        cookies: function() {
+                var filterForm = document.forms['upload-filter'],
+                    filterInput = filterForm['upload-filter'];
+
+                var restoreFormValueFromCookies = function(form) {
+                    var element;
+
+                    if (docCookies.hasItem(filterForm.elements['upload-filter'].value)) {
+                        console.log(filterForm.elements['upload-filter'].value);
+                        filterForm.elements['upload-filter'].value = docCookies.setItem(filterForm.elements['upload-filter'].value);
+                    }
+                    for (var i = 0; i < filterForm.elements['upload-filter'].length; i++) {
+
+                         element = filterForm.elements['upload-filter'].value;
+
+                    }
                 }
-            }
+
+            filterForm.onsubmit = function(evt) {
+                    evt.preventDefault();
+
+                     var element;
+
+                    console.log(filterForm.elements['upload-filter'].value);
+                    for (var i = 0; i < filterForm.elements['upload-filter'].length; i++) {
+                         //element = filterForm.elements['upload-filter'].value;
+                         //if (docCookies.hasItem(filterForm.elements['upload-filter'].value)) {
+                             docCookies.setItem(filterForm.elements['upload-filter'].value);
+                         //}
+                     }
+
+
+                    filterForm.submit();
+                }
+
+                restoreFormValueFromCookies(filterForm);
+        }
+
     };
 
-    inputSize(resizeX);
-    inputSize(resizeY);
-    inputSize(resizeSize);
 
 
-    //filterInput =  formFilter.filter;
-
-    /*var restoreFormValueCookie = function(form) {
-        var element;
-
-        for (var i = 0; i < formFilter.elements.length; i++) {
-            element = formFilter.elements[i];
 
 
-            if (docCookies.hasItem(element.name)) {
-                element.value = docCookies.setItem(element.value);
-            }
 
-        }
-    }*/
-
-
-/*    var elementForm;
-
-    formFilter.onsubmit = function(evt) {
-        evt.preventDefault();
-
-        for (var i = 0; i < formFilter.elements.length; i++) {
-            element = formFilter.elements[i];
-
-            docCookies.setItem(element.name, element.value);
-        }
-    }*/
+    (function() {
+        validation.init();
+    })();
 
 })();
