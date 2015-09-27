564f50343bcdd5aa563f9b73fd6730167d4dcb7e
Alexpri <Alexpri@github.com>
2015-09-27 19:58:27 +0300

    validation + cookie

--- js/form-validation.js
+++ js/form-validation.js
@@ -2,11 +2,33 @@
 
     var validation = {
 
-        init : function(){
+        init : function(image){
             var self = this;
-            console.log(1);
+            var formResize = document.forms['upload-resize'];
+            var filterForm = document.forms['upload-filter'];
+
             self.validation();
-            self.cookies();
+
+            formResize.onsubmit = function(evt) {
+                var x = parseInt(formResize["resize-x"].value, 10);
+                var y = parseInt(formResize["resize-y"].value, 10);
+                var size = parseInt(formResize["resize-size"].value, 10);
+
+                if (
+                    0 <= x && 0 <= y && 0 <= size &&
+                    x + size <= image.width &&
+                    y + size <= image.height
+                ) {
+                    evt.preventDefault();
+                    filterForm.elements['filter-image-src'] = image.src;
+
+                    formResize.classList.add('invisible');
+                    filterForm.classList.remove('invisible');
+                    self.cookies();
+                } else {
+                    evt.preventDefault();
+                }
+            };
         },
 
         validation: function () {
@@ -15,77 +37,86 @@
                 resizeY = formResize["resize-y"],
                 resizeSize = formResize["resize-size"],
                 inputsStep2 =  document.querySelectorAll(".upload-resize-controls input");
+            var image = formResize.querySelector('.resize-image-preview');
 
-            var MAX_PHOTO_SIDE = 569;
-            var MIN_PHOTO_SIDE = 0;
+            var MAX_PHOTO_SIDE = parseInt(Math.min(image.width, image.height));
+            var MIN_PHOTO_SIDE = 1;
             var MIN_INPUT_RESIZE = 0;
 
             resizeSize.min = MIN_PHOTO_SIDE;
             resizeSize.max = MAX_PHOTO_SIDE;
-            resizeSize.value = MAX_PHOTO_SIDE;
             resizeX.min = MIN_INPUT_RESIZE;
             resizeY.min = MIN_INPUT_RESIZE;
+
+            resizeSize.value = MAX_PHOTO_SIDE;
             resizeY.value = MIN_INPUT_RESIZE;
             resizeX.value = MIN_INPUT_RESIZE;
 
-            var inputSize = function (field) {
+            resizeX.onchange = function () {
+                resizeSize.value = MAX_PHOTO_SIDE - parseInt(resizeX.value, 10);
+            };
 
-                if (field == resizeX) {
+            resizeY.onchange = function () {
+                resizeSize.value = MAX_PHOTO_SIDE - parseInt(resizeY.value, 10);
+            };
 
-                    field.onchange = function () {
+            resizeSize.onchange = function () {
+                resizeX.value = MAX_PHOTO_SIDE - parseInt(resizeSize.value, 10);
+                resizeY.value = MAX_PHOTO_SIDE - parseInt(resizeSize.value, 10);
+            }
+        },
 
-                        resizeY.value = parseInt(field.value, 10);
-                        resizeSize.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
-                    }
-                } else if (field == resizeY) {
-                    field.onchange = function () {
+        cookies: function() {
+            var filterForm = document.forms['upload-filter'],
+                filterInput = filterForm['upload-filter'];
 
-                        resizeX.value = parseInt(field.value, 10);
-                        resizeSize.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
-                    }
-                }  else if (field == resizeSize) {
-                    field.onchange = function () {
+            var filterMap;
 
-                        resizeX.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
-                        resizeY.value = parseInt(MAX_PHOTO_SIDE - field.value, 10);
+            function setFilter(image, element) {
+                if (!filterMap) {
+                    filterMap = {
+                        'none': 'filter-none',
+                        'chrome': 'filter-chrome',
+                        'sepia': 'filter-sepia'
+                    };
+                }
+
+                console.log(image);
+                image.className = 'filter-image-preview' + ' ' + filterMap[element.value];
+            };
+
+            var restoreFormValueFromCookies = function(form) {
+                var name = 'upload-filter';
+                var filterEls = filterForm.elements[name];
+                var value = docCookies.getItem(name);
+                var image = document.forms["upload-filter"].querySelector('.filter-image-preview');
+
+                for (var i = 0; i < filterEls.length; i++) {
+
+                    var element = filterEls[i];
+                    if (value === element.value) {
+                        setFilter(image, element);
+
+                        element.checked = true;
                     }
                 }
             };
 
-            inputSize(resizeX);
-            inputSize(resizeY);
-            inputSize(resizeSize);
-        },
+            filterForm.onsubmit = function(evt) {
 
-        cookies: function() {
-                var filterForm = document.forms['upload-filter'],
-                    filterInput = filterForm['upload-filter'];
+                evt.preventDefault();
+                var name = 'upload-filter';
+                var filterEls = filterForm.elements[name];
+                for (var i = 0; i < filterEls.length; i++) {
+                    var element = filterEls[i];
 
-                var restoreFormValueFromCookies = function(form) {
-                    var element;
-
-                    for (var i = 0; i < filterForm.elements['upload-filter'].length; i++) {
-                         element = filterForm.elements['upload-filter'][i];
-
-                        if (docCookies.hasItem(element.name)) {
-                            console.log(element.name, element.checked);
-                            element.checked = docCookies.getItem(element.checked);
-                        }
+                    if (element.checked) {
+                        docCookies.setItem(name, element.value);
                     }
                 }
 
-            filterForm.onsubmit = function(evt) {
-                evt.preventDefault();
-
-                var element;
-                for (var i = 0; i < filterForm.elements['upload-filter'].length; i++) {
-                        element = filterForm.elements['upload-filter'][i];
-                        docCookies.setItem(element.name, element.checked);
-                        console.log(element.name, element.checked);
-                }
-
                 filterForm.submit();
-            }
+            };
 
             restoreFormValueFromCookies(filterForm);
         }
@@ -98,7 +129,12 @@
 
 
     (function() {
-        validation.init();
+        var formResize = document.forms["upload-resize"],
+            image = formResize.querySelector('.resize-image-preview');
+
+        image.onload = function() {
+            validation.init(image);
+        };
     })();
 
 })();
