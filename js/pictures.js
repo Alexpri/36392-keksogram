56956e3b236e86c382188b57581b268f8924a809
alexpr <alexpr@ronis.mail>
2015-09-30 21:06:24 +0300

    JS  image

--- js/pictures.js
+++ js/pictures.js
@@ -2,14 +2,41 @@
 	var pictureTemp = document.querySelector('#picture-template'),
 		pictureContainer = document.querySelector('.pictures');
 
+	var picturesFragment = document.createDocumentFragment();
+
+	var IMAGE_FAILURE_TIMEOUT = 10000;
+
 	pictures.forEach(function(picture, i) {
 		var newPictureElem = pictureTemp.content.children[0].cloneNode(true);
 
 		newPictureElem.querySelector('.picture-comments').textContent = picture['comments'];
 		newPictureElem.querySelector('.picture-likes').textContent = picture['likes'];
-		newPictureElem.querySelector('.picture-likes').setAttribute = picture['url'];
-		
 
-		console.log(picture, newPictureElem);
+
+		if (picture['url']) {
+			pictureImage = new Image();
+			pictureImage.src = picture['url'];
+
+			//console.log(pictureImage.src);
+				newPictureElem.querySelector('img').src = pictureImage.src;
+
+			var imageLoadTimeout = setTimeout(function() {
+					newPictureElem.parentNode.classList.add('picture-load-failure');
+				}, IMAGE_FAILURE_TIMEOUT);
+
+			pictureImage.onload = function() {
+				clearTimeout(imageLoadTimeout);
+			};
+
+			pictureImage.onerror = function() {
+				newPictureElem.classList.add('picture-load-failure');
+			};
+
+		}
+		
+		picturesFragment.appendChild(newPictureElem);
 	});
+
+	pictureContainer.appendChild(picturesFragment);
+
 })();
