ab0871323d0f3a6a218dc4ab32a88d3c59d1e930
Alexpri <Alexpri@github.com>
2015-10-01 08:00:52 +0300

    Picture js

--- js/pictures.js
+++ js/pictures.js
@@ -1,47 +1,63 @@
 (function() {
-	var pictureTemp = document.querySelector('#picture-template'),
-		pictureContainer = document.querySelector('.pictures');
 
-	var picturesFragment = document.createDocumentFragment();
+	var Template = {
+		init: function(){
+			self = this;
 
-	var IMAGE_FAILURE_TIMEOUT = 10000;
+			self.create();
+		},
 
-	pictures.forEach(function(picture, i) {
-		var newPictureElem = pictureTemp.content.children[0].cloneNode(true);
+		create: function() {
+			var pictureTemp = document.querySelector('#picture-template'),
+				pictureContainer = document.querySelector('.pictures');
 
-		newPictureElem.querySelector('.picture-comments').textContent = picture['comments'];
-		newPictureElem.querySelector('.picture-likes').textContent = picture['likes'];
+			var picturesFragment = document.createDocumentFragment();
+
+			var IMAGE_FAILURE_TIMEOUT = 10000;
+			var IMAGE_WIDTH = 182;
+			var IMAGE_HEIGHT = 182;
+
+			pictures.forEach(function(picture, i) {
+				var newPictureElem = pictureTemp.content.children[0].cloneNode(true);
+
+				newPictureElem.querySelector('.picture-comments').textContent = picture['comments'];
+				newPictureElem.querySelector('.picture-likes').textContent = picture['likes'];
 
 
-		if (picture['url']) {
-			var pictureImage = new Image();
+				if (picture['url']) {
+					var pictureImage = new Image();
 
-			pictureImage.src = picture['url'];
+					pictureImage.src = picture['url'];
 
-			var pictureImageOld = newPictureElem.querySelector('img');
+					var pictureImageOld = newPictureElem.querySelector('img');
 
-			pictureImage.parentNode.replaceChild(newChild, oldChild)
+					newPictureElem.querySelector('img').parentNode.replaceChild(pictureImage, pictureImageOld);
 
-			//console.log(pictureImage.src);
-			newPictureElem.querySelector('img').src = pictureImage.src;
 
-			var imageLoadTimeout = setTimeout(function() {
-					newPictureElem.parentNode.classList.add('picture-load-failure');
-				}, IMAGE_FAILURE_TIMEOUT);
+					var imageLoadTimeout = setTimeout(function() {
+						newPictureElem.classList.add('picture-load-failure');
+					}, IMAGE_FAILURE_TIMEOUT);
 
-			pictureImage.onload = function() {
-				clearTimeout(imageLoadTimeout);
-			};
+					pictureImage.onload = function() {
+						pictureImage.width = IMAGE_WIDTH;
+						pictureImage.height = IMAGE_HEIGHT;
+						clearTimeout(imageLoadTimeout);
+					};
 
-			pictureImage.onerror = function() {
-				newPictureElem.classList.add('picture-load-failure');
-			};
+					pictureImage.onerror = function() {
+						newPictureElem.classList.add('picture-load-failure');
+					};
 
+				}
+
+				picturesFragment.appendChild(newPictureElem);
+			});
+
+			pictureContainer.appendChild(picturesFragment);
 		}
-		
-		picturesFragment.appendChild(newPictureElem);
-	});
+	};
 
-	pictureContainer.appendChild(picturesFragment);
-
+	(function() {
+		Template.init();
+	})();
 })();
